4a87f4b5f6563bf6a7dcb59c6044bda1e29aa333
rBogdanCS <rosca.bogdan02@gmail.com>
2025-10-05 14:11:47 +0200

    add lint check

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'prettier',
+  ],
+  plugins: [
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+  },
+};
