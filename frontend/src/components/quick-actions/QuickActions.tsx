14ab1293ac2328c796d9f6076950262c04448762
rBogdanCS <rosca.bogdan02@gmail.com>
2025-10-05 12:50:17 +0200

    organise components

--- frontend/src/components/quick-actions/QuickActions.tsx
+++ frontend/src/components/quick-actions/QuickActions.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './QuickActions.css';
-import Categories from './Categories';
+import Categories from '../categories/Categories';
 
 interface QuickActionsProps {
   onActionClick?: (action: string) => void;
